afdba6cf47ba7ecfcfe6afa1e63367252665a7f2
ChoiJaeEun <poungki0823@gmail.com>
2021-11-12 11:33:14 +0900

    color dto, color classification

---
+++ capstone/src/main/java/cau/capstone/dto/color/ColorClassification.java
@@ -0,0 +1,12 @@
+package cau.capstone.dto.color;
+
+public enum ColorClassification {
+    RED,
+    ORANGE,
+    YELLOW,
+    GREEN,
+    BLUE,
+    PURPLE,
+    PINK,
+    WHITE
+}
---
+++ capstone/src/main/java/cau/capstone/dto/color/RGBColor.java
@@ -0,0 +1,25 @@
+package cau.capstone.dto.color;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class RGBColor implements Comparable {
+    private int r;
+    private int g;
+    private int b;
+    private double proportion;
+
+    @Override
+    public int compareTo(Object o) {
+        RGBColor rgbColor = (RGBColor) o;
+        if (this.proportion < rgbColor.proportion) {
+            return -1;
+        } else if (this.proportion == rgbColor.proportion) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+}
