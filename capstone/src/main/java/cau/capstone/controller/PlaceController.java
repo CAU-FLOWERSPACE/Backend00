7f3cd2532c80d93f7348d5ccb8997b332c71738f
ChoiJaeEun <poungki0823@gmail.com>
2021-11-14 20:44:01 +0900

    rename Controller 공간테라피

---
+++ capstone/src/main/java/cau/capstone/controller/PlaceController.java
@@ -0,0 +1,36 @@
+package cau.capstone.controller;
+
+import cau.capstone.api.dto.ApiRequest;
+import cau.capstone.dto.place.PlaceFlowerResponse;
+import cau.capstone.dto.place.PlacePlantInfoResponse;
+import cau.capstone.dto.place.PlacePlantResponse;
+import cau.capstone.service.PlaceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class PlaceController {
+
+    private final PlaceService placeService;
+
+    // 공간 분석 api 호출 restTemplate 에게 ApiRequest 넘겨주기
+    @PostMapping("/place/plant")  // image upload
+    public PlacePlantResponse postPlacePlants(@RequestBody ApiRequest apiRequest) {
+
+        return placeService.recommendPlacePlants(apiRequest);
+    }
+
+    @GetMapping("/place/plant/{plant_id}")
+    public PlacePlantInfoResponse getPlacePlant(@PathVariable Long plant_id) {
+
+        return placeService.getPlacePlantInfo(plant_id);
+    }
+
+    @PostMapping("/place/flower")
+    public PlaceFlowerResponse postPlaceFlowers(@RequestBody ApiRequest apiRequest) {
+
+        return placeService.recommandPlaceFlowers(apiRequest);
+    }
+}
