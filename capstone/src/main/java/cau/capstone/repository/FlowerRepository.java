e19dfdf00114efa795ecd14444ca1a14b5525888
Im-HyeJeong <dlaowjde@naver.com>
2021-11-14 16:23:37 +0900

    컬러테라피 쿼리 수정

--- capstone/src/main/java/cau/capstone/repository/FlowerRepository.java
+++ capstone/src/main/java/cau/capstone/repository/FlowerRepository.java
@@ -3,6 +3,7 @@ package cau.capstone.repository;
 import cau.capstone.domain.Flower;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,8 +11,9 @@ import java.util.Optional;
 
 public interface FlowerRepository extends JpaRepository<Flower, Long> {
 
-  @Query(nativeQuery = true, value = "SELECT * FROM Flower order by rand() LIMIT 5")
-  List<Flower> findByColor(String color);
+  @Query(nativeQuery = true, value = "SELECT * FROM Flower f WHERE f.color = :color order by rand() LIMIT 5")
+  List<Flower> findByColor(@Param("color") String color);
 
   Optional<Flower> findById(Long flower_id);
+
 }
