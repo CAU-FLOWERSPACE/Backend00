429afde6b5a992fcdb8e4465dd8754736bfababa
ChoiJaeEun <poungki0823@gmail.com>
2021-11-14 20:57:42 +0900

    find plant, flower

--- capstone/src/main/java/cau/capstone/repository/FlowerRepository.java
+++ capstone/src/main/java/cau/capstone/repository/FlowerRepository.java
@@ -1,6 +1,8 @@
 package cau.capstone.repository;
 
 import cau.capstone.domain.Flower;
+import cau.capstone.dto.color.ColorClassification;
+import cau.capstone.dto.place.PlaceFlowerResponse.FlowerResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,4 +18,12 @@ public interface FlowerRepository extends JpaRepository<Flower, Long> {
 
   Optional<Flower> findById(Long flower_id);
 
+  @Query(nativeQuery = true, value = "SELECT * FROM Flower where color = :color and s >= :s order by rand() LIMIT 2")
+  List<FlowerResponse> findByColorAndSGreaterThanEqual(ColorClassification color, int s);  // 공간이 진한 경우 채도 높은 꽃
+
+  @Query(nativeQuery = true, value = "SELECT * FROM Flower where color = :color and s < :s order by rand() LIMIT 2")
+  List<FlowerResponse> findByColorAndSLessThan(ColorClassification color, int s);  // 공간이 진한 경우 채도 낮은 꽃
+
+  @Query(nativeQuery = true, value = "SELECT * FROM Flower where color = :color order by rand() LIMIT 2")
+  List<FlowerResponse> findByColor(ColorClassification color);
 }
--- capstone/src/main/java/cau/capstone/repository/PlantRepository.java
+++ capstone/src/main/java/cau/capstone/repository/PlantRepository.java
@@ -1,13 +1,21 @@
 package cau.capstone.repository;
 
 import cau.capstone.domain.Plant;
+import cau.capstone.dto.place.PlacePlantInfoResponse;
+import cau.capstone.dto.place.PlacePlantResponse.PlantResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PlantRepository extends JpaRepository<Plant, Long> {
 
     @Query(nativeQuery = true, value = "SELECT * FROM Plant order by rand() LIMIT 5")
-    List<Plant> findPlantByPlace(String place);
+    List<PlantResponse> findPlantByPlace(String place);
+
+    PlacePlantInfoResponse findPlantById(Long id);
+
+
 }
