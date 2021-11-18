a6b12adf0b7dfae035cc0a31cec8988136e41463
ChoiJaeEun <poungki0823@gmail.com>
2021-11-19 01:24:18 +0900

    refactoring service

--- capstone/src/main/java/cau/capstone/service/ColorAlgorithm.java
+++ capstone/src/main/java/cau/capstone/service/ColorAlgorithm.java
@@ -2,7 +2,6 @@ package cau.capstone.service;
 
 import cau.capstone.dto.color.ColorClassification;
 import cau.capstone.dto.color.RGBColor;
-import org.springframework.stereotype.Service;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -12,23 +11,25 @@ import java.util.List;
 
 public class ColorAlgorithm {
 
-    List<RGBColor> RGBColors;
+    List<RGBColor> rgbColors;
     static float[] hsbValues;
 
     // BaseColor 의 채도와 명도
     static int BaseS;
 
     // ColorClassification Enum 리스트
-    private static final List<ColorClassification> values = new ArrayList<>(Arrays.asList(ColorClassification.values()));
+    private final List<ColorClassification> values;
 
     // 생성자
-    public ColorAlgorithm(List<RGBColor> RGBColors) {
-        RGBColors.sort(Collections.reverseOrder());
-        this.RGBColors = RGBColors.subList(0, 3);  // 3 번째 색상까지만 사용
+    public ColorAlgorithm(List<RGBColor> rgbColors) {
+        rgbColors.sort(Collections.reverseOrder());
+        this.rgbColors = rgbColors.subList(0, 3);  // 3 번째 색상까지만 사용
+        this.values = new ArrayList<>(Arrays.asList(ColorClassification.values()));
     }
 
     // BaseColor 채도 get method
     public int getBaseS() {
+
         return BaseS;
     }
 
@@ -36,31 +37,29 @@ public class ColorAlgorithm {
     public List<ColorClassification> recommendBaseColor() {
         // 추천할 color 를 담은 리스트
         List<ColorClassification> recommendColors;
-
         // RGB to HSV value
-        hsbValues = RGBToHSV(this.RGBColors.get(0));
+        hsbValues = RGBToHSV(this.rgbColors.get(0));
         int H = Math.round(hsbValues[0] * 360);
         BaseS = Math.round(hsbValues[1] * 100);  // baseColor 의 채도만 고려
-
         // H, S 가 모두 0 인 경우 -> r==g==b : 화이트 꽃 추천
         if (H == 0 && BaseS == 0) {
-            recommendColors = values.subList(7, 8);  // WHITE
+            recommendColors = new ArrayList<>(values.subList(7, 8));  // WHITE
             return recommendColors;
         }
 
-        // Enum 의 index 를 활용해서 base color 의 보색이 어디서부터 어디까지인지 결정한다.
+        // Enum 의 index 를 활용해서 base color 의 배색이 어디서부터 어디까지인지 결정한다.
         int startColorIdx = getColor((H + 330) % 360).ordinal();
         int endColorIdx = getColor((H + 30) % 360).ordinal();
 
         if (startColorIdx <= endColorIdx) {
-            recommendColors = values.subList(startColorIdx, endColorIdx+1);
+            recommendColors = new ArrayList<>(values.subList(startColorIdx, endColorIdx + 1));
         }
         else {  // 0 ~ end, start ~ 7
-            recommendColors = values.subList(0, endColorIdx+1);
+            recommendColors = new ArrayList<>(values.subList(0, endColorIdx+1));
             recommendColors.addAll(values.subList(startColorIdx, 7));
         }
 
-        // 추천 Color list return List<ColorClassification>
+        // base 색상 추천 return List<ColorClassification>
         return recommendColors;
     }
 
@@ -69,26 +68,27 @@ public class ColorAlgorithm {
         List<ColorClassification> recommendColors;
 
         // pointColor 1 RGB to HSV value
-        hsbValues = RGBToHSV(this.RGBColors.get(1));
+        hsbValues = RGBToHSV(this.rgbColors.get(1));
         int H1 = Math.round(hsbValues[0] * 360);
         int S1 = Math.round(hsbValues[1] * 100);
+
         // pointColor 2 RGB to HSV value
-        hsbValues =  RGBToHSV(this.RGBColors.get(2));
+        hsbValues =  RGBToHSV(this.rgbColors.get(2));
         int H2 = Math.round(hsbValues[0] * 360);
         int S2 = Math.round(hsbValues[1] * 100);
 
         // H1, S1 or H2, S2 가 0 인 경우 <- r==g==b
         if (H1 == 0 && S1 == 0) {
-            recommendColors = values.subList(7, 8);  // WHITE
+            recommendColors = new ArrayList<>(values.subList(7, 8));  // WHITE
         }
         else {
             int pointColorIdx = getColor((H1 + 180) % 360).ordinal();
-            recommendColors = values.subList(pointColorIdx, pointColorIdx+1);
+            recommendColors = new ArrayList<>(values.subList(pointColorIdx, pointColorIdx+1));
         }
 
         if (H1 != H2) {  // point 색상이 서로 다른 경우
             if (H2 == 0 && S2 == 0) {
-                recommendColors = values.subList(7, 8);  // WHITE
+                recommendColors.addAll(values.subList(7, 8));  // WHITE
             }
             else {
                 int pointColorIdx = getColor((H2 + 180) % 360).ordinal();
@@ -96,24 +96,19 @@ public class ColorAlgorithm {
             }
         }
 
-        // 추천 Color list return List<ColorClassification>
+        // point 색상 추천 return List<ColorClassification>
         return recommendColors;
     }
 
     // RGB 를 HSV 로 변경해주는 method
     private float[] RGBToHSV(RGBColor rgbColor) {
-        int R = rgbColor.getR();
-        int G = rgbColor.getG();
-        int B = rgbColor.getB();
 
-        return Color.RGBtoHSB(R, G, B, null);  // float[3] = [H, S, V]
+        return Color.RGBtoHSB(rgbColor.getR(), rgbColor.getG(), rgbColor.getB(), null);  // return float[3] = [H, S, V]
     }
 
     // H 의 범위에 따라 Color 를 분류해주는 method
     private ColorClassification getColor(int H) {
-//        if (H == 0) {
-//            return ColorClassification.WHITE;
-//        }  // 꽃이 흰색인 경우는 위에서 처리 됨
+        // 꽃이 흰색인 경우는 위에서 처리 됨
         if (348 <= H || H <= 12) {
             return ColorClassification.RED;
         }
--- capstone/src/main/java/cau/capstone/service/PlaceService.java
+++ capstone/src/main/java/cau/capstone/service/PlaceService.java
@@ -3,14 +3,17 @@ package cau.capstone.service;
 import cau.capstone.api.SpaceApiRestTemplate;
 import cau.capstone.api.dto.ApiRequest;
 import cau.capstone.api.dto.ColorApiResponse;
+import cau.capstone.domain.Flower;
+import cau.capstone.domain.Place;
+import cau.capstone.domain.Plant;
+import cau.capstone.domain.Recommend;
 import cau.capstone.dto.color.ColorClassification;
 import cau.capstone.dto.place.PlaceFlowerResponse;
 import cau.capstone.dto.place.PlaceFlowerResponse.FlowerResponse;
 import cau.capstone.dto.place.PlacePlantInfoResponse;
-import cau.capstone.dto.place.PlacePlantResponse;
 import cau.capstone.dto.place.PlacePlantResponse.PlantResponse;
-import cau.capstone.repository.FlowerRepository;
-import cau.capstone.repository.PlantRepository;
+import cau.capstone.dto.place.PlacePlantResponse;
+import cau.capstone.repository.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -19,52 +22,69 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
-@RequiredArgsConstructor
 @Service
+@RequiredArgsConstructor
 public class PlaceService {
 
     private final PlantRepository plantRepository;
     private final FlowerRepository flowerRepository;
+    private final PlaceRepository placeRepository;
+    private final RecommendRepository recommendRepository;
     private final SpaceApiRestTemplate spaceApiRestTemplate;
 
-    public PlacePlantResponse recommendPlacePlants(ApiRequest apiRequest) {
-        String place = spaceApiRestTemplate.placeAPI(apiRequest).getPlace();
-        List<PlantResponse> plantResponseList = plantRepository.findPlantByPlace(place);  // place 활용 repo 로 넘겨주기
+    public PlacePlantResponse recommendPlacePlantList(ApiRequest apiRequest) {
+        Place place = placeRepository.findByPlace(spaceApiRestTemplate.placeAPI(apiRequest).getPlace());
+        List<Recommend> recommendList = recommendRepository.findRecommendsByPlace(place);
+        List<PlantResponse> plantResponses = new ArrayList<>();
 
-        return new PlacePlantResponse(place, plantResponseList);
+        for (Recommend entity : recommendList) {
+            plantResponses.add(new PlantResponse(entity.getPlant()));
+        }
+
+        return new PlacePlantResponse(place.getPlace(), plantResponses);
     }
 
     public PlacePlantInfoResponse getPlacePlantInfo(Long id) {
+        Plant plant = plantRepository.findById(id).get();
 
-        return plantRepository.findPlantById(id);
+        return new PlacePlantInfoResponse(plant);
     }
 
-    public PlaceFlowerResponse recommandPlaceFlowers(ApiRequest apiRequest) {
+    public PlaceFlowerResponse recommendPlaceFlowerList(ApiRequest apiRequest) {
         // ColorAlgoService 사용!
         ColorApiResponse colorApiResponse = spaceApiRestTemplate.colorAPI(apiRequest);
         ColorAlgorithm colorAlgorithm = new ColorAlgorithm(colorApiResponse.getRgbColors());
 
         // base 색상 기반 추천
         List<ColorClassification> recommandBaseColorList = colorAlgorithm.recommendBaseColor();
-
+        // point 색상 기반 추천
         List<ColorClassification> recommandPointColorList = colorAlgorithm.recommendPointColor();
 
+        List<Flower> flowerList = new ArrayList<>();
         List<FlowerResponse> flowerResponses = new ArrayList<>();
 
         // base color 에 맞는 꽃 리스트
         for (ColorClassification color: recommandBaseColorList) {
             if (colorAlgorithm.getBaseS() < 50) {
-                flowerResponses.addAll(flowerRepository.findByColorAndSGreaterThanEqual(color, 50));
+                flowerList.addAll(flowerRepository.findByColorAndSGreaterThanEqual(color.toString(), 50));
             } else {
-                flowerResponses.addAll(flowerRepository.findByColorAndSLessThan(color,50));
+                flowerList.addAll(flowerRepository.findByColorAndSLessThan(color.toString(),50));
             }
         }
 
-        // base color 에 맞는 꽃 리스트
+        // point color 에 맞는 꽃 리스트
         for (ColorClassification color: recommandPointColorList) {
-            flowerResponses.addAll(flowerRepository.findByColor(color));
+//            flowerList.addAll(flowerRepository.findFlowerByColor(color.toString()));
+            if (colorAlgorithm.getBaseS() < 50) {
+                flowerList.addAll(flowerRepository.findByColorAndSGreaterThanEqual(color.toString(), 50));
+            } else {
+                flowerList.addAll(flowerRepository.findByColorAndSLessThan(color.toString(),50));
+            }
         }
 
+        for(Flower entity : flowerList) {
+            flowerResponses.add(new FlowerResponse(entity));
+        }
         return new PlaceFlowerResponse(flowerResponses);
     }
 }
