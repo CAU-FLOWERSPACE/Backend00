571a3b2f757e2cdaba6a604cfad99b9550a569b8
Im-HyeJeong <dlaowjde@naver.com>
2021-11-25 00:15:01 +0900

    color 중복 제거

--- capstone/src/main/java/cau/capstone/service/PlaceService.java
+++ capstone/src/main/java/cau/capstone/service/PlaceService.java
@@ -19,23 +19,25 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class PlaceService {
 
-    private final PlantRepository plantRepository;
-    private final FlowerRepository flowerRepository;
-    private final PlaceRepository placeRepository;
-    private final RecommendRepository recommendRepository;
-    private final SpaceApiRestTemplate spaceApiRestTemplate;
+  private final PlantRepository plantRepository;
+  private final FlowerRepository flowerRepository;
+  private final PlaceRepository placeRepository;
+  private final RecommendRepository recommendRepository;
+  private final SpaceApiRestTemplate spaceApiRestTemplate;
 
-    public PlacePlantResponse recommendPlacePlantList(ApiRequest apiRequest) {
-        Place place = placeRepository.findByPlace(spaceApiRestTemplate.placeAPI(apiRequest).getPlace());
-        List<Recommend> recommendList = recommendRepository.findRecommendsByPlace(place);
-        List<PlantResponse> plantResponses = new ArrayList<>();
+  public PlacePlantResponse recommendPlacePlantList(ApiRequest apiRequest) {
+    Place place = placeRepository.findByPlace(spaceApiRestTemplate.placeAPI(apiRequest).getPlace());
+    List<Recommend> recommendList = recommendRepository.findRecommendsByPlace(place);
+    List<PlantResponse> plantResponses = new ArrayList<>();
 
         for (Recommend entity : recommendList) {
             plantResponses.add(new PlantResponse(entity.getPlant()));
@@ -56,41 +58,63 @@ public class PlaceService {
         return new PlacePlantInfoResponse(plant);  // , placeResponses
     }
 
-    public PlaceFlowerResponse recommendPlaceFlowerList(ApiRequest apiRequest) {
-        // ColorAlgoService 사용!
-        ColorApiResponse colorApiResponse = spaceApiRestTemplate.colorAPI(apiRequest);
-        ColorAlgorithm colorAlgorithm = new ColorAlgorithm(colorApiResponse.getRgbColors());
+  public PlaceFlowerResponse recommendPlaceFlowerList(ApiRequest apiRequest) {
+    // ColorAlgoService 사용!
+    ColorApiResponse colorApiResponse = spaceApiRestTemplate.colorAPI(apiRequest);
+    ColorAlgorithm colorAlgorithm = new ColorAlgorithm(colorApiResponse.getRgbColors());
 
-        // base 색상 기반 추천
-        List<ColorClassification> recommandBaseColorList = colorAlgorithm.recommendBaseColor();
-        // point 색상 기반 추천
-        List<ColorClassification> recommandPointColorList = colorAlgorithm.recommendPointColor();
+    // base 색상 기반 추천
+    List<ColorClassification> recommandBaseColorList = colorAlgorithm.recommendBaseColor();
 
-        List<Flower> flowerList = new ArrayList<>();
-        List<FlowerResponse> flowerResponses = new ArrayList<>();
+    // point 색상 기반 추천
+    List<ColorClassification> recommandPointColorList = colorAlgorithm.recommendPointColor();
 
-        // base color 에 맞는 꽃 리스트
-        for (ColorClassification color: recommandBaseColorList) {
-            if (colorAlgorithm.getBaseS() < 50) {
-                flowerList.addAll(flowerRepository.findByColorAndSGreaterThanEqual(color.toString(), 50));
-            } else {
-                flowerList.addAll(flowerRepository.findByColorAndSLessThan(color.toString(),50));
-            }
-        }
+    List<Flower> flowerList = new ArrayList<>();
+    List<FlowerResponse> flowerResponses = new ArrayList<>();
 
-        // point color 에 맞는 꽃 리스트
-        for (ColorClassification color: recommandPointColorList) {
-//            flowerList.addAll(flowerRepository.findFlowerByColor(color.toString()));
-            if (colorAlgorithm.getBaseS() < 50) {
-                flowerList.addAll(flowerRepository.findByColorAndSGreaterThanEqual(color.toString(), 50));
-            } else {
-                flowerList.addAll(flowerRepository.findByColorAndSLessThan(color.toString(),50));
-            }
-        }
+    Map<String, Boolean> usedColors = new HashMap<>();
 
-        for(Flower entity : flowerList) {
-            flowerResponses.add(new FlowerResponse(entity));
-        }
-        return new PlaceFlowerResponse(flowerResponses);
+    // base color 에 맞는 꽃 리스트
+    for (ColorClassification color: recommandBaseColorList) {
+
+      // 교차중복도 걸러주고 같은 그룹안에 있는 중복도 걸러줌
+      if ( usedColors.containsKey(color.toString()) ) {
+          continue;
+      }
+
+      usedColors.put(color.toString(), true);
+
+      System.out.println(color.toString());
+
+      if (colorAlgorithm.getBaseS() < 50) {
+        flowerList.addAll(flowerRepository.findByColorAndSGreaterThanEqual(color.toString(), 50));
+      } else {
+        flowerList.addAll(flowerRepository.findByColorAndSLessThan(color.toString(),50));
+      }
     }
+
+    // point color 에 맞는 꽃 리스트
+    for (ColorClassification color: recommandPointColorList) {
+      if ( usedColors.containsKey(color.toString()) ) {
+        continue;
+      }
+
+      usedColors.put(color.toString(), true);
+
+      System.out.println(color.toString());
+
+//    flowerList.addAll(flowerRepository.findFlowerByColor(color.toString()));
+
+      if (colorAlgorithm.getBaseS() < 50) {
+        flowerList.addAll(flowerRepository.findByColorAndSGreaterThanEqual(color.toString(), 50));
+      } else {
+        flowerList.addAll(flowerRepository.findByColorAndSLessThan(color.toString(),50));
+      }
+    }
+
+    for(Flower entity : flowerList) {
+      flowerResponses.add(new FlowerResponse(entity));
+    }
+    return new PlaceFlowerResponse(flowerResponses);
+  }
 }
