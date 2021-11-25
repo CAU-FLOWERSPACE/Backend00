1621d881ef227b0282bbad219813996f74c5605e
ChoiJaeEun <poungki0823@gmail.com>
2021-11-25 14:18:52 +0900

    add ar setting

--- capstone/src/main/java/cau/capstone/config/WebConfig.java
+++ capstone/src/main/java/cau/capstone/config/WebConfig.java
@@ -1,8 +1,12 @@
 package cau.capstone.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -13,5 +17,12 @@ public class WebConfig implements WebMvcConfigurer {
       .allowedOrigins("*")
       .allowedHeaders("*")
       .allowedMethods("*");
+  }
+
+  @Override
+  public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/**")
+            .addResourceLocations("classpath:/templates/", "classpath:/static")
+            .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
   }
 }
