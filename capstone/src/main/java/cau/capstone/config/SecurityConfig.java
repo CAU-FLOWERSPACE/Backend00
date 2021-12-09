cb5674766c4cfc2caef81e9375ce839b3cf3cc19
Im-HyeJeong <dlaowjde@naver.com>
2021-12-10 01:18:42 +0900

    SecurityConfig (1. test1 로 변경)

--- capstone/src/main/java/cau/capstone/config/SecurityConfig.java
+++ capstone/src/main/java/cau/capstone/config/SecurityConfig.java
@@ -27,14 +27,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+// test1
+
 @Transactional(readOnly = true)
 @Configuration // IoC에 등록, 메모리에 떠야한다.
 @EnableWebSecurity // 해당 파일로 시큐리티를 활성화한다.
 @Slf4j
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-//  @Autowired
-//  private CustomOAuth2UserService customOAuth2UserService;
 
   @Bean
   public PasswordEncoder passwordEncoder(){
@@ -46,45 +46,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
-      .csrf().disable()
       .authorizeRequests()
-//      .antMatchers("/api/ar").authenticated() // AR page 는 로그인한 사용자만 접속 가능 *** ar url 확인하기
-//      .antMatchers("/", "/api/user/signup").permitAll()
-        .anyRequest().permitAll()
-        .and()
+      // AR page 는 인증이 필요함
+      .anyRequest().permitAll()
+      .and()
       .formLogin()
-        .usernameParameter("email")
-        .passwordParameter("password")
-        .successHandler(new AuthenticationSuccessHandler() {
-          @Override
-          public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-            log.info("[login success]");
+      .usernameParameter("email")
+      .passwordParameter("password")
+      .successHandler(new AuthenticationSuccessHandler() {
+        @Override
+        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+          log.info("[login success]");
 //            response.sendRedirect("/api/test");
-            response.setStatus(HttpStatus.OK.value());
-          }
-        })
-        .failureHandler(new AuthenticationFailureHandler() {
-          @Override
-          public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-            log.error("로그인 실패");
-            response.setStatus(HttpStatus.UNAUTHORIZED.value());
-          }
-        })
-        .and()
-      .logout()
-      .logoutUrl("/logout")
-      .permitAll()
-      .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK))
-//      .deleteCookies("JSESSIONID") // 특정 쿠키 제거
-//      .invalidateHttpSession(true) // HTTP Session 초기화
+        }
+      })
+      .failureHandler(new AuthenticationFailureHandler() {
+        @Override
+        public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+          log.error("로그인 실패");
+          response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        }
+      })
       .and()
       .cors()
-        .configurationSource(corsConfigurationSource());
-//      .and()
-//      .logout()
-//     .invalidateHttpSession(true); // HTTP Session 초기화
-//     .deleteCookies("JSESSIONID"); // 특정 쿠키 제거
-
+      .configurationSource(corsConfigurationSource())
+      .and()
+      .csrf().disable();
 
 //    http
 //      .oauth2Login()
@@ -92,6 +79,75 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //      .userService(customOAuth2UserService);
 
   }
+
+
+//////기존 ///////////
+
+//@Transactional(readOnly = true)
+//@Configuration // IoC에 등록, 메모리에 떠야한다.
+//@EnableWebSecurity // 해당 파일로 시큐리티를 활성화한다.
+//@Slf4j
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+////  @Autowired
+////  private CustomOAuth2UserService customOAuth2UserService;
+//
+//  @Bean
+//  public PasswordEncoder passwordEncoder(){
+//    return new BCryptPasswordEncoder();
+//  }
+//
+//
+//
+//  @Override
+//  protected void configure(HttpSecurity http) throws Exception {
+//    http
+//      .csrf().disable()
+//      .authorizeRequests()
+////      .antMatchers("/api/ar/**").authenticated() // AR page 는 로그인한 사용자만 접속 가능 *** ar url 확인하기
+////      .antMatchers("/", "/api/user/signup").permitAll()
+//        .anyRequest().permitAll()
+//        .and()
+//      .formLogin()
+//        .usernameParameter("email")
+//        .passwordParameter("password")
+//        .successHandler(new AuthenticationSuccessHandler() {
+//          @Override
+//          public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+//            log.info("[login success]");
+////            response.sendRedirect("/api/test");
+//            response.setStatus(HttpStatus.OK.value());
+//          }
+//        })
+//        .failureHandler(new AuthenticationFailureHandler() {
+//          @Override
+//          public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+//            log.error("로그인 실패");
+//            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+//          }
+//        })
+//        .and()
+//      .logout()
+//      .logoutUrl("/logout")
+//      .permitAll()
+//      .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK))
+////      .deleteCookies("JSESSIONID") // 특정 쿠키 제거
+////      .invalidateHttpSession(true) // HTTP Session 초기화
+//      .and()
+//      .cors()
+//        .configurationSource(corsConfigurationSource());
+////      .and()
+////      .logout()
+////     .invalidateHttpSession(true); // HTTP Session 초기화
+////     .deleteCookies("JSESSIONID"); // 특정 쿠키 제거
+//
+//
+////    http
+////      .oauth2Login()
+////      .userInfoEndpoint()
+////      .userService(customOAuth2UserService);
+//
+//  }
 
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
