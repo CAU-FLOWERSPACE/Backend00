a2f288a3e69b8fc400020157285ca0b8b9fa86ed
Im-HyeJeong <dlaowjde@naver.com>
2021-12-09 16:33:21 +0900

    WebConfig (Origins -> OriginPatterns 변경)

--- capstone/src/main/java/cau/capstone/config/WebConfig.java
+++ capstone/src/main/java/cau/capstone/config/WebConfig.java
@@ -14,7 +14,8 @@ public class WebConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
       .allowCredentials(true)
-      .allowedOrigins("*")
+      .allowedOriginPatterns("*")
+//      .allowedOrigins("*")
       .allowedHeaders("*")
       .allowedMethods("*");
   }
